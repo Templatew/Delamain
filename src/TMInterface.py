619630aaddb2dbb3077dd91bdb2525987a37b22a
Templatew <lorenz@aorus.localdomain>
2025-12-07 19:30:46 +0100

    Removed unused messages

--- src/TMInterface.py
+++ src/TMInterface.py
@@ -9,19 +9,15 @@ class TestClient(Client):
         
     def on_registered(self, iface: TMInterface):
         print("Connected to TMInterface!")
-        print("Waiting for race steps...")
-        print()
-        # Set a longer timeout so we don't disconnect
+        print("Waiting for race steps...\n")
         iface.set_timeout(10000)
         
     def on_run_step(self, iface: TMInterface, time: int):
         """Called during normal gameplay - try to inject inputs"""
         self.step_count += 1
         
-        # Always press accelerate!
         iface.set_input_state(accelerate=True)
         
-        # Print every second
         if self.step_count % 100 == 0:
             try:
                 state = iface.get_simulation_state()
@@ -39,7 +35,6 @@ class TestClient(Client):
         """Called during replay validation - full control here!"""
         self.step_count += 1
         
-        # Full control in simulation mode
         iface.set_input_state(accelerate=True, brake=False, left=False, right=False)
         
         if self.step_count % 100 == 0:
@@ -58,17 +53,7 @@ def main():
     print("=" * 50)
     print("TMInterface Control Test")
     print("=" * 50)
-    print()
-    print("This script will TRY to control your car.")
-    print()
-    print("For GUARANTEED control:")
-    print("  1. Watch a replay and click 'Validate'")  
-    print("  2. This triggers SIMULATION mode")
-    print("  3. The script will have full control")
-    print()
-    print("Press Ctrl+C to exit")
-    print()
-    
+
     client = TestClient()
     run_client(client, "TMInterface0")
 
